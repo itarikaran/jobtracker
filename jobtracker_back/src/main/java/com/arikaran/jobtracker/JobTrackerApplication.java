984bb993b064f0b1d76688e0455d9ac4e43b299a
Ari Karan <arikaran4044@gmail.com>
2026-08-25 15:12:12 +0530

    db connection, tailwind config

--- jobtracker_back/src/main/java/com/arikaran/jobtracker/JobTrackerApplication.java
+++ jobtracker_back/src/main/java/com/arikaran/jobtracker/JobTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobtrackerApplication {
+public class JobTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobtrackerApplication.class, args);
+		SpringApplication.run(JobTrackerApplication.class, args);
 	}
 
 }
--- jobtracker_back/src/test/java/com/arikaran/jobtracker/JobTrackerApplicationTests.java
+++ jobtracker_back/src/test/java/com/arikaran/jobtracker/JobTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JobtrackerApplicationTests {
+class JobTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
